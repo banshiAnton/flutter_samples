8ddf6cfd4c4b26813c8d2e0478f1eddc23c4f27d
AntonSeleznev <anton@connectycube.com>
2020-05-09 18:20:12 +0300

    point to free stun/turn server

--- flutter_janus/lib/janus.config.dart
+++ flutter_janus/lib/janus.config.dart
@@ -2,17 +2,16 @@ const janusConfig = const {
   'server': 'wss://janus.conf.meetecho.com/ws',
   'quality': 'hires',
   'iceServers': [
-        {"url": "stun:stun.l.google.com:19302"},
-        {"url": 'stun:turn.connectycube.com'},
-        {
-          "url": 'turn:turn.connectycube.com:5349?transport=udp',
-          "username": 'connectycube',
-          "credential": '4c29501ca9207b7fb9c4b4b6b04faeb1'
-        },
-        {
-          "url": 'turn:turn.connectycube.com:5349?transport=tcp',
-          "username": 'connectycube',
-          "credential": '4c29501ca9207b7fb9c4b4b6b04faeb1'
-        }
+      {"url": "stun:stun.l.google.com:19302"},
+      {
+        "url": 'turn:numb.viagenie.ca',
+        "credential": 'muazkh',
+        "username": 'webrtc@live.com'
+      },
+      {
+        "url": 'turn:turn.anyfirewall.com:443?transport=tcp',
+        "credential": 'webrtc',
+        "username": 'webrtc'
+      }
     ]
 };
